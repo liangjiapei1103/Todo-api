e870ed7ca644385bce1f28fafab86acba84928d7
Jiapei Liang <liang98@purdue.edu>
2016-01-26 08:49:32 -0500

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -45,18 +46,24 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	db.todo.create(body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function () {
+		res.status(400).json(e);
+	});
 
-	body.description = body.description.trim();
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
-	// add id field
-	body.id = todoNextId++;
-	// push body into array
-	todos.push(body);
+	// body.description = body.description.trim();
 
-	res.json(body);
+	// // add id field
+	// body.id = todoNextId++;
+	// // push body into array
+	// todos.push(body);
+
+	// res.json(body);
 });
 
 // DELETE /todos/:id
@@ -102,7 +109,9 @@ app.put('/todos/:id', function (req, res) {
 
 });
 
-
-app.listen(PORT, function () {
-	console.log('Express listening on port ' + PORT +'!');
+db.sequelize.sync().then(function () {
+	app.listen(PORT, function () {
+		console.log('Express listening on port ' + PORT +'!');
+	});
 });
+
