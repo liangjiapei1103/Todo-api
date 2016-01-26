d8b5e20a48b10c1aa2b0fa675ea3d67f7f144f8a
Jiapei Liang <liang98@purdue.edu>
2016-01-25 22:27:35 -0500

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+// 	name: 'Jiapei',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Jiapei',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades = [15, 88];
+
+function addGrades (grades) {
+	// grades.push(55);
+	grades = [12, 33, 99];
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
